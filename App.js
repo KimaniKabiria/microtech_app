16692cbba05384c96b37f9f2b95351a89f6a83dd
Kimani Kabiria <kabiriakimani@gmail.com>
2020-05-03 16:22:54 +0300

    Added Keyboard Dismiss & Editted AbouUs page

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './screens/home';
 import Help from './screens/help';
 import About from './screens/about';
-import { Text, View } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -29,39 +29,43 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <TouchableWithoutFeedback onPress={() =>{
+      Keyboard.dismiss();
+    }}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-list-box' 
-                : 'ios-list';
-            } else if (route.name === 'About') {
-              iconName = focused 
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if(route.name === 'Help') {
-              iconName = focused
-                ? 'ios-help-circle'
-                : 'ios-help-circle-outline'
-            }
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'ios-list-box' 
+                  : 'ios-list';
+              } else if (route.name === 'About') {
+                iconName = focused 
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline';
+              } else if(route.name === 'Help') {
+                iconName = focused
+                  ? 'ios-help-circle'
+                  : 'ios-help-circle-outline'
+              }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'deepskyblue',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Help" component={HelpScreen} />
-        <Tab.Screen name="About" component={AboutScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'deepskyblue',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Help" component={HelpScreen} />
+          <Tab.Screen name="About" component={AboutScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </TouchableWithoutFeedback>
   );
 }
--- screens/about.js
+++ screens/about.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.introText}>We are:</Text>
+      <Image
+        style={{ width: 300, height: 56 }}
+        source = {require('../assets/splash.png')}
+      />
+      <Text style={{padding: 10}}>You can contact us through the following:</Text>
+      <View style={styles.contact}>
+        <FontAwesome name="phone-square" size={24} style={styles.contactIcon} />
+        <Text style={styles.contactText}>0738 252 333</Text>
+      </View>
+      <View style={styles.contact}>
+        <Ionicons name="md-mail" size={24} style={styles.contactIcon} />
+        <Text style={styles.contactText}>microtechcreations@gmail.com</Text>
+      </View>
+      <View style={styles.copy}>
+        <FontAwesome name="copyright" size={14} />
+        <Text style={{marginLeft: 10}}>Microtech</Text>
+      </View>
     </View>
   );
 }
@@ -15,5 +33,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  introText:{
+    fontWeight: 'bold',
+    fontSize: 32,
+    padding: 10,
+  },
+  contact:{
+    flexDirection: 'row',
+  },
+  contactIcon:{
+    padding: 5,
+  },
+  contactText:{
+    padding: 5,
+    fontWeight: '300',
+    fontSize: 24,
+  },
+  copy:{
+    flexDirection: 'row',
+    paddingTop: 200,
   },
 });
